9481874c29d71aca112ed12835d494686948f5c9
flixgithub <lxd-best@163.com>
2017-07-13 13:37:52 +0800

    add STATIC_ROOT to settings

--- ithub/settings.py
+++ ithub/settings.py
@@ -132,6 +132,7 @@ USE_TZ = False
 STATIC_URL = '/static/'
 # 其它 存放静态文件的文件夹，可以用来存放项目中公用的静态文件，里面不能包含 STATIC_ROOT
 # 如果不想用 STATICFILES_DIRS 可以不用，都放在 app 里的 static 中也可以
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "common_static"),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, "common_static"),
+# )
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
